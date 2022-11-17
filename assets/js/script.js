494dd9c84e842034afee79219ae0c8f62d99803e
LDudrey <113869466+LDudrey@users.noreply.github.com>
2022-11-16 23:23:18 -0600

    able to call city name and return data to console log

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
-var APIKey = "db2abe03dbd181408bf857199d38b427/";
+var APIKey = "db2abe03dbd181408bf857199d38b427";
 var cityName;
+var cityLat;
+var cityLon;
 var citySearchEl = document.getElementById('search-input');
 var searchButtonEl = document.querySelector('#submit-btn');
 var requestUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}';
@@ -12,67 +14,126 @@ var requestUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon=
 var searchSubmit = function (event) {
     event.preventDefault();
 
-    cityName = citySearchEl.value.trim();
+    var cityName = citySearchEl.value.trim();
     console.log(cityName)
-    if (cityName) {
-        getCityName(cityName);
 
-        repoContainerEl.textContent = '';
+    if (cityName) {
+        getCityLocation(cityName);
         citySearchEl.value = '';
     } else {
         alert('Please enter a city name');
     }
 };
 
-// var buttonClickHandler 
-var pastSearchButton = function (event) {
-    var prevCities = document.querySelector;
-  
-    if (prevCities) {
-        getCityName(prevCities);
-  
-      repoContainerEl.textContent = '';
-    }
-  };
+// var getUserRepos
+// Country code https://www.iso.org/obp/ui/#search/code/
+var getCityLocation = function (city) {
+    var geoRequestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + ',840&limit=1&appid=' + APIKey;
+    
+    fetch(geoRequestUrl)
+    .then(function (response) {
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function (data) {
+          console.log(data.lat);
+          displayRepos(lat, lon);
+        });
+      } else {
+        alert('Error: ' + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert('City does not exist');
+    });
+};
 
-// var getCityName = function () {
-//     var geoCurrentUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + ',&limit=1&appid=' + APIKey;
-//     cityName = userQuery;
-//     if (cityName) {
-//         citySearchEl.textContent = cityName;
-//         getCityLocation(cityName);
-//     } else {
-//         alert('City does not exist');
-//     }
-//     console.log(cityName)
+// var getCityWeather = function (city) {
+//     var geoRequestUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + APIKey;
+
+
 // };
 
-var getCityName = function (user) {
-    var geoCurrentUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + ',&limit=1&appid=' + APIKey;
+//var displayRepos
+// var displayForecast = function (repos, searchTerm) {
+//     if (repos.length === 0) {
+//       repoContainerEl.textContent = 'No repositories found.';
+//       return;
+//     }
+
+//     repoSearchTerm.textContent = searchTerm;
+
+//   for (var i = 0; i < repos.length; i++) {
+//     var repoName = repos[i].owner.login + '/' + repos[i].name;
+
+//     var repoEl = document.createElement('a');
+//     repoEl.classList = 'list-item flex-row justify-space-between align-center';
+//     repoEl.setAttribute('href', './single-repo.html?repo=' + repoName);
+
+//     var titleEl = document.createElement('span');
+//     titleEl.textContent = repoName;
+
+//     repoEl.appendChild(titleEl);
+
+//     var statusEl = document.createElement('span');
+//     statusEl.classList = 'flex-row align-center';
+
+//     if (repos[i].open_issues_count > 0) {
+//       statusEl.innerHTML =
+//         "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
+//     } else {
+//       statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+//     }
+
+//     repoEl.appendChild(statusEl);
+
+//     repoContainerEl.appendChild(repoEl);
+//   }
+// };
+
+
+// // var buttonClickHandler 
+// var pastSearchButton = function (event) {
+//     var prevCities = document.querySelector;
   
-    fetch(geoCurrentUrl)
-      .then(function (response) {
-        if (response.ok) {
-          console.log(response);
-          response.json().then(function (data) {
-            console.log(data);
-            displayRepos(data, user);
-          });
-        } else {
-          alert('Error: ' + response.statusText);
-        }
-      })
-      .catch(function (error) {
-        alert('City does not exist');
-      });
-  };
+
+
+
+
+//     // var displayIssues
+// var displayPastSearch = function (cities) {
+
+
+
+
+
+
+
+// var getCityName = function (user) {
+//     var geoCurrentUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + ',&limit=1&appid=' + APIKey;
+  
+//     fetch(geoCurrentUrl)
+//       .then(function (response) {
+//         if (response.ok) {
+//           console.log(response);
+//           response.json().then(function (data) {
+//             console.log(data);
+//             displayRepos(data, user);
+//           });
+//         } else {
+//           alert('Error: ' + response.statusText);
+//         }
+//       })
+//       .catch(function (error) {
+//         alert('City does not exist');
+//       });
+//   };
 
 
 // var geoCurrent = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit=5&appid={API key};
 // var geoCurrentUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + ',&limit=1&appid=' + APIKey;
 
-searchButtonEl.addEventListener('submit', searchSubmit);
-
+searchButtonEl.addEventListener('click', searchSubmit);
+pastSearchEl.addEventListener('click', buttonClickHandler);
 
 
 
