6540a83f50e293bc1082283101713271d6cfc03a
LDudrey <113869466+LDudrey@users.noreply.github.com>
2022-11-18 20:31:04 -0600

    able to retrieve weather icon relevant to current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 var APIKey = "db2abe03dbd181408bf857199d38b427";
 var cityInput;
+var cityNameDate = document.getElementById('name-date');
 var cityLat = "";
 var cityLon = "";
+var currentIconEl = document.getElementById('current-icon');
 var currentTempEl = document.getElementById('curtemp');
 var currentWindEl = document.getElementById('curwind');
 var currentHumidEl = document.getElementById('curhumid');
@@ -40,10 +42,10 @@ var getCityLocation = function (city) {
                     if (data.length === 0) {
                         alert('City does not exist!');
                     } else {
-                    cityLat = data[0].lat.toString();
-                    cityLon = data[0].lon.toString();
-                    getCurrentWeather();
-                    getFutureWeather();
+                        cityLat = data[0].lat.toString();
+                        cityLon = data[0].lon.toString();
+                        getCurrentWeather();
+                        getFutureWeather();
                     }
                 });
             } else {
@@ -57,93 +59,95 @@ var getCityLocation = function (city) {
 
 // Gets cities current weather by lat and lon coordinates
 var getCurrentWeather = function (city) {
-     var geoCurrentUrl = 'http://api.openweathermap.org/data/2.5/weather?lat=' + cityLat + '&lon=' + cityLon + '&appid=' + APIKey + "&units=imperial";
-     
-     fetch(geoCurrentUrl)
-     .then(function (response) {
-         if (response.ok) {
-             console.log(response);
-             response.json().then(function (data) {
-                 console.log(data); 
-                //  displayWeather();
-                 cityName = data.name;
-                var currentTemp = data.main.temp;
-                var currentWind = data.wind.speed;
-                var currentHumid = data.main.humidity;
-                currentTempEl.textContent = "Temp: " + currentTemp;
-                currentWindEl.textContent = "Wind: " +currentWind;
-                currentHumidEl.textContent = "Humidity: " + currentHumid;
-                 console.log(data.name);
-                 console.log(data.main.temp);
-                 console.log(data.wind.speed);
-                 console.log(data.main.humidity);            
-             });
-         } else {
-             alert('Error: ' + response.statusText);
-         }
-     })
-     .catch(function (error) {
-         alert('Unable to connect to Open Weather');
-     });
+    var geoCurrentUrl = 'http://api.openweathermap.org/data/2.5/weather?lat=' + cityLat + '&lon=' + cityLon + '&appid=' + APIKey + "&units=imperial";
+
+    fetch(geoCurrentUrl)
+        .then(function (response) {
+            if (response.ok) {
+                console.log(response);
+                response.json().then(function (data) {
+                    console.log(data);
+                    //  displayWeather();
+                    var cityName = data.name;
+                    var currentTemp = data.main.temp;
+                    var currentWind = data.wind.speed;
+                    var currentHumid = data.main.humidity;
+                    var currentIcon = "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
+                    currentTempEl.textContent = "Temp: " + currentTemp;
+                    currentWindEl.textContent = "Wind: " + currentWind;
+                    currentHumidEl.textContent = "Humidity: " + currentHumid;
+                    currentIconEl.src = currentIcon;
+                });
+            } else {
+                alert('Error: ' + response.statusText);
+            }
+        })
+        .catch(function (error) {
+            alert('Unable to connect to Open Weather');
+        });
 };
 
 // // Gets cities future weather by lat and lon coordinates
-// var getFutureWeather = function (city) {
-//     var geoCurrentUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + cityLat + '&lon=' + cityLon + '&appid=' + APIKey + "&units=imperial";
-    
-//     fetch(geoCurrentUrl)
-//     .then(function (response) {
-//         if (response.ok) {
-//             console.log(response);
-//             response.json().then(function (data) {
-//                 console.log(data); 
-//                 cityName = city.name;
+var getFutureWeather = function (city) {
+    var geoCurrentUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + cityLat + '&lon=' + cityLon + '&appid=' + APIKey + "&units=imperial";
 
-//             });
-//         } else {
-//             alert('Error: ' + response.statusText);
-//         }
-//     })
-//     .catch(function (error) {
-//         alert('Unable to connect to Open Weather');
-//     });
-// };
+    fetch(geoCurrentUrl)
+        .then(function (response) {
+            if (response.ok) {
+                console.log(response);
+                response.json().then(function (data) {
+                    console.log(data);
+                    cityName = city.name;
+                    console.log(data.name);
+                    console.log(data.main.temp);
+                    console.log(data.wind.speed);
+                    console.log(data.main.humidity);
+                    console.log(currentIconEl);
+                });
+            } else {
+                alert('Error: ' + response.statusText);
+            }
+        })
+        .catch(function (error) {
+            alert('Unable to connect to Open Weather');
+        });
+};
 
 //var displayRepos
 var displayWeather = function (repos, searchTerm) {
     if (repos.length === 0) {
-      repoContainerEl.textContent = 'No repositories found.';
-      return;
+        repoContainerEl.textContent = 'No repositories found.';
+        return;
     }
 
     repoSearchTerm.textContent = searchTerm;
 
-  for (var i = 0; i < repos.length; i++) {
-    var repoName = repos[i].owner.login + '/' + repos[i].name;
+    for (var i = 0; i < repos.length; i++) {
+        var repoName = repos[i].owner.login + '/' + repos[i].name;
 
-    var repoEl = document.createElement('a');
-    repoEl.classList = 'list-item flex-row justify-space-between align-center';
-    repoEl.setAttribute('href', './single-repo.html?repo=' + repoName);
+        var repoEl = document.createElement('a');
+        repoEl.classList = 'list-item flex-row justify-space-between align-center';
+        repoEl.setAttribute('href', './single-repo.html?repo=' + repoName);
 
-    var titleEl = document.createElement('span');
-    titleEl.textContent = repoName;
+        var titleEl = document.createElement('span');
+        titleEl.textContent = repoName;
 
-    repoEl.appendChild(titleEl);
+        repoEl.appendChild(titleEl);
 
-    var statusEl = document.createElement('span');
-    statusEl.classList = 'flex-row align-center';
+        var statusEl = document.createElement('span');
+        statusEl.classList = 'flex-row align-center';
 
-    if (repos[i].open_issues_count > 0) {
-      statusEl.innerHTML =
-        "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
-    } else {
-      statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+        if (repos[i].open_issues_count > 0) {
+            statusEl.innerHTML =
+                "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
+        } else {
+            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+        }
+
+        repoEl.appendChild(statusEl);
+
+        repoContainerEl.appendChild(repoEl);
     }
-
-    repoEl.appendChild(statusEl);
-
-    repoContainerEl.appendChild(repoEl);
-  }
 };
 
 
